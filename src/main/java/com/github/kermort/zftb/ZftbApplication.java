03bc59ac952b5ad93677487f9136616eca85a640
Kermort <64635345+Kermort@users.noreply.github.com>
2022-08-19 11:57:47 +0300

    Add files via upload

---
+++ src/main/java/com/github/kermort/zftb/ZftbApplication.java
@@ -0,0 +1,13 @@
+package com.github.kermort.zftb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZftbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZftbApplication.class, args);
+	}
+
+}
